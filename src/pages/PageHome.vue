d0c1326da2718996f146f79f0447b95c6f216b50
tgrbtrfly <pagevpetty@gmail.com>
2020-08-16 13:55:37 -0600

    31. Add a Posts Array to Data Object

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -13,8 +13,9 @@ https://lh3.googleusercontent.com/ogw/ADGmqu9_8g0o3hZm25AO3Ji1n2h5f2Zv9g3NocYpgM
 
         <q-item-section>
           <q-item-label
-          class="text-bold"
-          >tgrbtrfly</q-item-label>
+            class="text-bold"
+          >tgrbtrfly
+          </q-item-label>
           <q-item-label caption>
             Kingdom of God
           </q-item-label>
@@ -22,10 +23,12 @@ https://lh3.googleusercontent.com/ogw/ADGmqu9_8g0o3hZm25AO3Ji1n2h5f2Zv9g3NocYpgM
       </q-item>
 
       <q-separator/>
-      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+      <img src=
+             "https://cdn.quasar.dev/img/mountains.jpg"
+      >
 
       <q-card-section>
-        <div >My Heavenly Father Loves Me</div>
+        <div>My Heavenly Father Loves Me</div>
         <div class="text-caption text-grey">16 Aug 2020 1:45PM</div>
       </q-card-section>
 
@@ -36,7 +39,38 @@ https://lh3.googleusercontent.com/ogw/ADGmqu9_8g0o3hZm25AO3Ji1n2h5f2Zv9g3NocYpgM
 
 <script>
 export default {
-  name: 'PageHome'
+  name: 'PageHome',
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: 'My Heavenly Father Loves Me',
+          date: 1597607469739,
+          location: 'Kingdom of God',
+          imageURL: "https://cdn.quasar.dev/img/mountains.jpg"
+        },{
+          id: 2,
+          caption: 'My Heavenly Father Loves Me',
+          date: 1597607469739,
+          location: 'Kingdom of God',
+          imageURL: "https://cdn.quasar.dev/img/mountains.jpg"
+        },{
+          id: 3,
+          caption: 'My Heavenly Father Loves Me',
+          date: 1597607469739,
+          location: 'Kingdom of God',
+          imageURL: "https://cdn.quasar.dev/img/mountains.jpg"
+        },{
+          id: 4,
+          caption: 'My Heavenly Father Loves Me',
+          date: 1597607469739,
+          location: 'Kingdom of God',
+          imageURL: "https://cdn.quasar.dev/img/mountains.jpg"
+        },
+      ]
+    }
+  }
 }
 </script>
 
