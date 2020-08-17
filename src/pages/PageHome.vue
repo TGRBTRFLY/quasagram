2d52af24ca401b74a53c1dc6c6721b62f4d7a88f
tgrbtrfly <pagevpetty@gmail.com>
2020-08-16 22:47:19 -0600

    34. Add a Mini-Profile for Desktop

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,43 +1,70 @@
 <template>
   <q-page class="constrain q-pa-md">
 
-    <q-card
-      v-for="post in posts"
-      :key="post.id"
-      class="card-post q-mb-md"
-      flat
-      bordered
-    >
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="
+    <div class="row q-col-gutter-lg">
+      <div class="col-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="
 https://lh3.googleusercontent.com/ogw/ADGmqu9_8g0o3hZm25AO3Ji1n2h5f2Zv9g3NocYpgM59=s83-c-mo
 ">
-          </q-avatar>
-        </q-item-section>
+              </q-avatar>
+            </q-item-section>
 
-        <q-item-section>
-          <q-item-label
-            class="text-bold"
-          >tgrbtrfly
-          </q-item-label>
-          <q-item-label caption>
-            {{ post.location }}
-          </q-item-label>
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              <q-item-label
+                class="text-bold"
+              >tgrbtrfly
+              </q-item-label>
+              <q-item-label caption>
+                {{ post.location }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <q-separator/>
-      <img :src="post.imageURL"/>
+          <q-separator/>
+          <img :src="post.imageURL"/>
 
-      <q-card-section>
-        <div>{{ post.caption }}</div>
-        <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
-      </q-card-section>
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
+          </q-card-section>
 
 
-    </q-card>
+        </q-card>
+      </div>
+      <div class="col-4">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="
+https://lh3.googleusercontent.com/ogw/ADGmqu9_8g0o3hZm25AO3Ji1n2h5f2Zv9g3NocYpgM59=s83-c-mo
+">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label
+              class="text-bold"
+            >tgrbtrfly
+            </q-item-label>
+            <q-item-label caption>
+              Page Petty
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
+
+
   </q-page>
 </template>
 
