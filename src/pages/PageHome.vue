a5bde73d7b7eb5ade502b9b277f50e07d036c40c
tgrbtrfly <pagevpetty@gmail.com>
2020-08-19 16:06:38 -0600

    6. Sort Posts by Date

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,107 +1,134 @@
-<template >
+<template>
   <q-page class="constrain q-pa-md">
 
     <div class="row q-col-gutter-lg">
-      <div class="col-12 col-sm-8 ">
-        <q-card
-            v-for="post in posts"
-            :key="post.id"
-            class="card-post q-mb-md"
-            flat
-            bordered
-        >
-          <q-item >
-            <q-item-section avatar>
-              <q-avatar >
-                <img
-                    src="
-https://lh3.googleusercontent.com/ogw/ADGmqu9_8g0o3hZm25AO3Ji1n2h5f2Zv9g3NocYpgM59=s83-c-mo
-"
-                >
-              </q-avatar >
-            </q-item-section >
+      <div class="col-12 col-sm-8">
+        <template v-if="!loadingPosts && posts.length">
+          <q-card
+              v-for="post in posts"
+              :key="post.id"
+              class="card-post q-mb-md"
+              bordered
+              flat
+          >
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://s.gravatar.com/avatar/ce7f3697e231df38b3ca6065848520da?s=80">
+                </q-avatar>
+              </q-item-section>
 
-            <q-item-section >
-              <q-item-label
-                  class="text-bold"
-              >tgrbtrfly
-              </q-item-label >
-              <q-item-label caption>
-                {{ post.location }}
-              </q-item-label >
-            </q-item-section >
-          </q-item >
+              <q-item-section>
+                <q-item-label class="text-bold">danny__connell</q-item-label>
+                <q-item-label caption>
+                  {{ post.location }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
 
-          <q-separator />
-          <img :src="post.imageURL" />
+            <q-separator />
 
-          <q-card-section >
-            <div >{{ post.caption }}</div >
-            <div class="text-caption text-grey">{{ post.date | niceDate }}</div >
-          </q-card-section >
+            <q-img
+                :src="post.imageUrl"
+            />
 
-        </q-card >
-      </div >
+            <q-card-section>
+              <div>{{ post.caption }}</div>
+              <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
+            </q-card-section>
+
+          </q-card>
+        </template>
+        <template v-else-if="!loadingPosts && !posts.length">
+          <h5 class="text-center text-grey">No posts yet.</h5>
+        </template>
+        <template v-else>
+          <q-card flat bordered>
+            <q-item>
+              <q-item-section avatar>
+                <q-skeleton type="QAvatar" animation="fade" size="40px" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>
+                  <q-skeleton type="text" animation="fade" />
+                </q-item-label>
+                <q-item-label caption>
+                  <q-skeleton type="text" animation="fade" />
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-skeleton height="200px" square animation="fade" />
+
+            <q-card-section>
+              <q-skeleton type="text" class="text-subtitle2" animation="fade" />
+              <q-skeleton type="text" width="50%" class="text-subtitle2" animation="fade" />
+            </q-card-section>
+          </q-card>
+        </template>
+      </div>
+
       <div class="col-4 large-screen-only">
         <q-item class="fixed">
           <q-item-section avatar>
             <q-avatar size="48px">
-              <img
-                  src="
-https://lh3.googleusercontent.com/ogw/ADGmqu9_8g0o3hZm25AO3Ji1n2h5f2Zv9g3NocYpgM59=s83-c-mo
-"
-              >
-            </q-avatar >
-          </q-item-section >
+              <img src="https://s.gravatar.com/avatar/ce7f3697e231df38b3ca6065848520da?s=80">
+            </q-avatar>
+          </q-item-section>
 
-          <q-item-section >
-            <q-item-label
-                class="text-bold"
-            >tgrbtrfly
-            </q-item-label >
+          <q-item-section>
+            <q-item-label class="text-bold">danny__connell</q-item-label>
             <q-item-label caption>
-              Page Petty
-            </q-item-label >
-          </q-item-section >
-        </q-item >
-      </div >
-    </div >
+              Danny Connell
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
 
-  </q-page >
-</template >
+  </q-page>
+</template>
 
-<script >
-import {date} from 'quasar'
+<script>
+import { date } from 'quasar'
 
 export default {
   name: 'PageHome',
   data() {
     return {
-      posts: []
+      posts: [],
+      loadingPosts: false
     }
   },
   methods: {
     getPosts() {
+      this.loadingPosts = true
       this.$axios.get('http://localhost:3000/posts').then(response => {
         this.posts = response.data
+        this.loadingPosts = false
       }).catch(err => {
-        console.log('err:', err)
+        this.$q.dialog({
+          title: 'Error',
+          message: 'Could not find your location.'
+        })
+        this.loadingPosts = false
       })
     }
   },
   filters: {
     niceDate(value) {
-      return date.formatDate(value, 'YYYY MMM DD , h:mmA')
+      return date.formatDate(value, 'MMMM D h:mmA')
     }
   },
   created() {
     this.getPosts()
   }
 }
-</script >
+</script>
 
 <style lang="sass">
 .card-post
   .q-img
     min-height: 200px
-</style >
+</style>
