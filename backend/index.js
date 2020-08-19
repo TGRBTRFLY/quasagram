72beb74dbfd99e84497861b6c55e3eb8258c3025
tgrbtrfly <pagevpetty@gmail.com>
2020-08-19 14:19:45 -0600

    75. Display the Posts on the Home Page

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,46 @@
 /*
-    dependencies
- */
-const express = require( 'express' )
-const admin = require( 'firebase-admin' );
-const serviceAccount = require( './path/to/serviceAccountKey.json' );
-admin.initializeApp( {
-    credential: admin.credential.cert( serviceAccount )
-} );
-const db = admin.firestore();
+  dependencies
+*/
+
+const express = require('express')
+const admin = require('firebase-admin');
+
 /*
-    config - express
- */
+  config - express
+*/
+
 const app = express()
+
 /*
-    config - firebase
- */
-const serviceAccount = require( './serviceAccountKey.json' );
-admin.initializeApp( {
-    credential: admin.credential.cert( serviceAccount )
-} );
+  config - firebase
+*/
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 const db = admin.firestore();
+
 /*
-    endpoint - posts
- */
-app.get( '/posts', (request, response) => {
-    let posts = [
-        {
-            caption: 'Kolob',
-            location: 'Kingdom'
-        },
-        {
-            caption: 'London Eye',
-            location: 'London'
-        }
-    ]
-    response.send( posts )
-} )
+  endpoint - posts
+*/
+
+app.get('/posts', (request, response) => {
+    response.set('Access-Control-Allow-Origin', '*')
+
+    let posts = []
+    db.collection('posts').orderBy('date', 'desc').get().then(snapshot => {
+        snapshot.forEach((doc) => {
+            posts.push(doc.data())
+        });
+        response.send(posts)
+    })
+})
+
 /*
-    listen
- */
-app.listen( process.env.PORT || 3000 )
+  listen
+*/
+
+app.listen(process.env.PORT || 3000)
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,120 +1,107 @@
-<template>
+<template >
   <q-page class="constrain q-pa-md">
 
     <div class="row q-col-gutter-lg">
       <div class="col-12 col-sm-8 ">
         <q-card
-          v-for="post in posts"
-          :key="post.id"
-          class="card-post q-mb-md"
-          flat
-          bordered
+            v-for="post in posts"
+            :key="post.id"
+            class="card-post q-mb-md"
+            flat
+            bordered
         >
-          <q-item>
+          <q-item >
             <q-item-section avatar>
-              <q-avatar>
-                <img src="
+              <q-avatar >
+                <img
+                    src="
 https://lh3.googleusercontent.com/ogw/ADGmqu9_8g0o3hZm25AO3Ji1n2h5f2Zv9g3NocYpgM59=s83-c-mo
-">
-              </q-avatar>
-            </q-item-section>
+"
+                >
+              </q-avatar >
+            </q-item-section >
 
-            <q-item-section>
+            <q-item-section >
               <q-item-label
-                class="text-bold"
+                  class="text-bold"
               >tgrbtrfly
-              </q-item-label>
+              </q-item-label >
               <q-item-label caption>
                 {{ post.location }}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
+              </q-item-label >
+            </q-item-section >
+          </q-item >
 
-          <q-separator/>
-          <img :src="post.imageURL"/>
+          <q-separator />
+          <img :src="post.imageURL" />
 
-          <q-card-section>
-            <div>{{ post.caption }}</div>
-            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
-          </q-card-section>
+          <q-card-section >
+            <div >{{ post.caption }}</div >
+            <div class="text-caption text-grey">{{ post.date | niceDate }}</div >
+          </q-card-section >
 
-
-        </q-card>
-      </div>
+        </q-card >
+      </div >
       <div class="col-4 large-screen-only">
         <q-item class="fixed">
           <q-item-section avatar>
             <q-avatar size="48px">
-              <img src="
+              <img
+                  src="
 https://lh3.googleusercontent.com/ogw/ADGmqu9_8g0o3hZm25AO3Ji1n2h5f2Zv9g3NocYpgM59=s83-c-mo
-">
-            </q-avatar>
-          </q-item-section>
+"
+              >
+            </q-avatar >
+          </q-item-section >
 
-          <q-item-section>
+          <q-item-section >
             <q-item-label
-              class="text-bold"
+                class="text-bold"
             >tgrbtrfly
-            </q-item-label>
+            </q-item-label >
             <q-item-label caption>
               Page Petty
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-      </div>
-    </div>
+            </q-item-label >
+          </q-item-section >
+        </q-item >
+      </div >
+    </div >
 
+  </q-page >
+</template >
 
-  </q-page>
-</template>
-
-<script>
+<script >
 import {date} from 'quasar'
 
 export default {
   name: 'PageHome',
   data() {
     return {
-      posts: [
-        {
-          id: 1,
-          caption: 'My Heavenly Father Loves Me',
-          date: 1597607469739,
-          location: 'Kingdom of God',
-          imageURL:
-            "https://cdn.quasar.dev/img/mountains.jpg"
-        }, {
-          id: 2,
-          caption: 'My Heavenly Father Loves Me',
-          date: 1597607469739,
-          location: 'Kingdom of God',
-          imageURL: "https://cdn.quasar.dev/img/mountains.jpg"
-        }, {
-          id: 3,
-          caption: 'My Heavenly Father Loves Me',
-          date: 1597607469739,
-          location: 'Kingdom of God',
-          imageURL: "https://cdn.quasar.dev/img/mountains.jpg"
-        }, {
-          id: 4,
-          caption: 'My Heavenly Father Loves Me',
-          date: 1597607469739,
-          location: 'Kingdom of God',
-          imageURL: "https://cdn.quasar.dev/img/mountains.jpg"
-        },
-      ]
+      posts: []
+    }
+  },
+  methods: {
+    getPosts() {
+      this.$axios.get('http://localhost:3000/posts').then(response => {
+        this.posts = response.data
+      }).catch(err => {
+        console.log('err:', err)
+      })
     }
   },
   filters: {
     niceDate(value) {
       return date.formatDate(value, 'YYYY MMM DD , h:mmA')
     }
+  },
+  created() {
+    this.getPosts()
   }
 }
-</script>
+</script >
 
 <style lang="sass">
 .card-post
   .q-img
     min-height: 200px
-</style>
+</style >
