25213cb01ef1d745277ce2582b1c63dd97e1749f
tgrbtrfly <pagevpetty@gmail.com>
2020-08-18 22:46:58 -0600

    73. Connect to the Firestore Database

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,24 @@
     dependencies
  */
 const express = require( 'express' )
+const admin = require( 'firebase-admin' );
+const serviceAccount = require( './path/to/serviceAccountKey.json' );
+admin.initializeApp( {
+    credential: admin.credential.cert( serviceAccount )
+} );
+const db = admin.firestore();
 /*
     config - express
  */
 const app = express()
+/*
+    config - firebase
+ */
+const serviceAccount = require( './serviceAccountKey.json' );
+admin.initializeApp( {
+    credential: admin.credential.cert( serviceAccount )
+} );
+const db = admin.firestore();
 /*
     endpoint - posts
  */
@@ -21,7 +35,7 @@ app.get( '/posts', (request, response) => {
         }
     ]
     response.send( posts )
-})
+} )
 /*
     listen
  */
